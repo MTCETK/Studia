91b1d017c5e4d5449c2aa85b612e23e659f8c4dc
kacperwiercinski <81072864+kacperwiercinski@users.noreply.github.com>
2021-04-25 09:46:03 +0200

    Część projektu kalkulator 'działania'

---
+++ dzialania.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QLabel, QGridLayout
+from PyQt5.QtWidgets import QLineEdit, QPushButton, QHBoxLayout
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtCore import Qt
+
+
+class Kalkulator(QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.interfejs()
+
+    def interfejs(self):
+
+        self.setLayout(ukladT)
+
+        koniecBtn.clicked.connect(self.koniec)
+        dodajBtn.clicked.connect(self.dzialanie)
+        odejmijBtn.clicked.connect(self.dzialanie)
+        mnozBtn.clicked.connect(self.dzialanie)
+        dzielBtn.clicked.connect(self.dzialanie)
+
+
+    def dzialanie(self):
+
+        nadawca = self.sender()
+
+        try:
+            liczba1 = float(self.liczba1Edt.text())
+            liczba2 = float(self.liczba2Edt.text())
+            wynik = ""
+
+            if nadawca.text() == "&Dodaj":
+                wynik = liczba1 + liczba2
+            else:
+                pass
+
+            self.wynikEdt.setText(str(wynik))
+
+        except ValueError:
+            QMessageBox.warning(self, "Błąd", "Błędne dane", QMessageBox.Ok)
+
+    if nadawca.text() == "&Dodaj":
+        wynik = liczba1 + liczba2
+    elif nadawca.text() == "&Odejmij":
+        wynik = liczba1 - liczba2
+    elif nadawca.text() == "&Mnóż":
+        wynik = liczba1 * liczba2
+    else:  # dzielenie
+        try:
+            wynik = round(liczba1 / liczba2, 9)
+        except ZeroDivisionError:
+            QMessageBox.critical(
+                self, "Błąd", "Nie można dzielić przez zero!")
+            return

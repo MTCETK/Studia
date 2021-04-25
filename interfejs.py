3c4513fcdfe32c8b016c6470234076aea81db721
Dominik Marcinkowski <81073040+mlodynorton@users.noreply.github.com>
2021-04-25 10:20:56 +0200

    interfejs

---
+++ interfejs.py
@@ -0,0 +1,74 @@
+from PyQt5.QtWidgets import QLineEdit, QPushButton, QHBoxLayout
+
+class Kalkulator(QWidget):
+
+    def interfejs(self):
+
+
+        # etykiety
+        etykieta1 = QLabel("Liczba 1:", self)
+        etykieta2 = QLabel("Liczba 2:", self)
+        etykieta3 = QLabel("Wynik:", self)
+
+        # przypisanie widgetów do układu tabelarycznego
+        ukladT = QGridLayout()
+        ukladT.addWidget(etykieta1, 0, 0)
+        ukladT.addWidget(etykieta2, 0, 1)
+        ukladT.addWidget(etykieta3, 0, 2)
+
+        # 1-liniowe pola edycyjne
+        self.liczba1Edt = QLineEdit()
+        self.liczba2Edt = QLineEdit()
+        self.wynikEdt = QLineEdit()
+
+        self.wynikEdt.readonly = True
+        self.wynikEdt.setToolTip('Wpisz <b>liczby</b> i wybierz działanie...')
+
+        ukladT.addWidget(self.liczba1Edt, 1, 0)
+        ukladT.addWidget(self.liczba2Edt, 1, 1)
+        ukladT.addWidget(self.wynikEdt, 1, 2)
+
+        # przyciski
+        dodajBtn = QPushButton("&Dodaj", self)
+        odejmijBtn = QPushButton("&Odejmij", self)
+        dzielBtn = QPushButton("&Mnóż", self)
+        mnozBtn = QPushButton("D&ziel", self)
+        koniecBtn = QPushButton("&Koniec", self)
+        koniecBtn.resize(koniecBtn.sizeHint())
+
+        ukladH = QHBoxLayout()
+        ukladH.addWidget(dodajBtn)
+        ukladH.addWidget(odejmijBtn)
+        ukladH.addWidget(dzielBtn)
+        ukladH.addWidget(mnozBtn)
+
+        ukladT.addLayout(ukladH, 2, 0, 1, 3)
+        ukladT.addWidget(koniecBtn, 3, 0, 1, 3)
+        # 1-liniowe pola edycyjne
+        self.liczba1Edt = QLineEdit()
+        self.liczba2Edt = QLineEdit()
+        self.wynikEdt = QLineEdit()
+
+        self.wynikEdt.readonly = True
+        self.wynikEdt.setToolTip('Wpisz <b>liczby</b> i wybierz działanie...')
+
+        ukladT.addWidget(self.liczba1Edt, 1, 0)
+        ukladT.addWidget(self.liczba2Edt, 1, 1)
+        ukladT.addWidget(self.wynikEdt, 1, 2)
+
+        # przyciski
+        dodajBtn = QPushButton("&Dodaj", self)
+        odejmijBtn = QPushButton("&Odejmij", self)
+        dzielBtn = QPushButton("&Mnóż", self)
+        mnozBtn = QPushButton("D&ziel", self)
+        koniecBtn = QPushButton("&Koniec", self)
+        koniecBtn.resize(koniecBtn.sizeHint())
+
+        ukladH = QHBoxLayout()
+        ukladH.addWidget(dodajBtn)
+        ukladH.addWidget(odejmijBtn)
+        ukladH.addWidget(dzielBtn)
+        ukladH.addWidget(mnozBtn)
+
+        ukladT.addLayout(ukladH, 2, 0, 1, 3)
+        ukladT.addWidget(koniecBtn, 3, 0, 1, 3)
